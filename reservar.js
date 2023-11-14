a13e8bab735e8215987f4fe4dbd7c5ad0452e186
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2023-11-14 12:13:48 -0300

    actualizando

--- reservar.js
+++ reservar.js
@@ -1,12 +1,44 @@
-console.log("EL DOM se ha cargado");
-document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelector('form').addEventListener('submit', function (event) {
-        event.preventdefault();
+//Código de destinos
 
-        var destino = document.getElementById('destino').value;
-        var fechaVuelo = document.getElementById('fechaVuelo').value;
+document.addEventListener("DOMContentLoaded", () => {
 
-        alert('Has seleccionado el destino: ' + destino + '\nFecha del vuelo: ' + fechaVuelo);
-    });
-});
+var boton = document.getElementById('boton')
+    var destino = document.getElementById('destino');
+    var fechaVuelo = document.getElementById('fechaVuelo');
+    var exito = document.getElementById("alertaExito");
+    var advertencia = document.getElementById("alertaAdvertencia")
+
+
+    fetch("MOCK_DATA (3)[1].json")
+    .then(response => {
+            console.log('entro al fetch')
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('No se pudo cargar el archivo JSON.');
+            }
+        })
+        .then(dataViajes => {
+            dataViajes.slice(0,50).forEach(element => {
+                destino.innerHTML += `<option>${element.destination}</option>`
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar el archivo JSON:', error);
+        });
+    boton.addEventListener("click", () => {
+        if (fechaVuelo.value) {
+            exito.style.display = "block";
+            setTimeout(() => {
+                exito.style.display = "none"; 
+            },3000)
+        } else {
+            advertencia.style.display = "block";
+            setTimeout(() => {
+                advertencia.style.display = "none";
+            }, 3000)
+        }
+    })
+
+})
