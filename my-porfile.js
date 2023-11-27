64477f022a1bcdaa5ca73d3472264607f86d72bb
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2023-11-27 10:44:43 -0300

    Terminando perfil de usuario

---
+++ my-porfile.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", () => {
+    validaciones();
+    mostrarImagen();
+});
+
+
+function validaciones() {
+    const form = document.getElementById("form");
+    const datos = document.querySelectorAll(".data");
+    const imagenPrevia = document.getElementById('imagenUsuario');
+    imagenPrevia.src = localStorage.getItem('imagenUsuarioSrc');
+    let datosObtenidos = JSON.parse(localStorage.getItem('datosUsuario'));
+    const fileInput = document.getElementById('fotoPerfil');
+
+    form.addEventListener("submit", (event) => {
+
+        //Agustín punto 1.2
+        // Guardar la URL de la imagen en el localStorage
+        localStorage.setItem('imagenUsuarioSrc', imagenPrevia.src);
+        //Si no se selecciona ninguna imagen, carga la imagen por defecto
+        if (fileInput.files.length === 0) {
+            localStorage.setItem('imagenUsuarioSrc', 'usuarioDefecto.webp');
+            mostrarImagen()
+        }
+
+        //Verifica si el formulario está validado
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            let datosStorage = [];
+            datos.forEach((dato) => {
+                datosStorage.push(dato.value);
+            })
+            localStorage.setItem('datosUsuario', JSON.stringify(datosStorage));
+            event.preventDefault();
+            console.log(localStorage.getItem('datosUsuario'));
+            form.classList.add("was-validated");
+        }
+
+    })
+
+    datos.forEach((input, index) => {
+        if (index === 4) {
+            input.value = localStorage.getItem('user');
+        } else {
+            input.value = datosObtenidos[index];
+        }
+    })
+}
+
+function mostrarImagen() {
+    const fileInput = document.getElementById('fotoPerfil');
+    const imagenPrevia = document.getElementById('imagenUsuario');
+
+    const archivo = fileInput.files[0]; // Obtener el archivo seleccionado
+    imagenPrevia.src = localStorage.getItem('imagenUsuarioSrc')
+    if (archivo) {
+        const lector = new FileReader(); // Crear un lector de archivos
+
+        lector.onload = function (event) {
+            // Verifica si el archivo cargado es una imagen
+            if (archivo.type.startsWith('image/')) {
+                imagenPrevia.src = event.target.result; // Mostrar la imagen en el elemento <img>
+
+                console.log(localStorage.getItem('imagenUsuarioSrc'));
+            } else {
+                console.log('El archivo seleccionado no es una imagen.');
+            }
+        }
+        lector.readAsDataURL(archivo); // Leer el archivo como una URL de datos
+    }
+}
+
